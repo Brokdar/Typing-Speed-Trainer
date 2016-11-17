3a7a7e4027efcfc042660723b524d4dcd991a9e4
Blubb <Blubb@BLUBB>
2016-11-18 00:12:17 +0100

    Create example GUI

---
+++ Typing Speed Trainer/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Typing_Speed_Trainer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
